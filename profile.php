13053b1a5f05e9e743584c2234d517f3fd080a6a
Eduardo Zardain <ejzardainc@gmail.com>
2015-11-04 13:36:07 -0600

    Agrega el profile

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lalo
+ * Date: 4/11/15
+ * Time: 11:51 AM
+ */
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="../osham/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Perfil</title>
+
+</head>
+<body>
+<div class="container">
+    <div class="col-md-8">
+        <h1>Nombre y Apellidos</h1>
+        <p>Aqui va el bio</p>
+    </div>
+    <div class="col-md-4">
+        <div class="container"">
+            <img style="max-height: 250px" src="../osham/images/placeholder.jpeg" class="img-responsive img-circle">
+        </div>
+    </div>
+</div>
+</body>
+</html>
