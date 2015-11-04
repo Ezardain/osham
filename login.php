c28513b8bb9394068cb4671ca47d2c6e34bce4a2
Eduardo Zardain <ejzardainc@gmail.com>
2015-11-04 11:45:51 -0600

    Initial commit

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lalo
+ * Date: 4/11/15
+ * Time: 11:07 AM
+ */
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="../osham/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Login</title>
+
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <form class="form-signin">
+                <h3>Iniciar Sesion</h3>
+                <label for="email">Correo Electronico</label>
+                <input type="email" id="inputEmail" class="form-control">
+                <label for="password">Password</label>
+                <input type="password" id="inputPassword" class="form-control">
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lalo
+ * Date: 4/11/15
+ * Time: 11:24 AM
+ */
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="../osham/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Registro</title>
+
+</head>
+<body>
+<div class="container">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <form class="form-signin">
+            <h3>Registrate</h3>
+            <label for="name">Nombre</label>
+            <input name="name" id="inputName" class="form-control">
+
+            <label for="email">Correo Electronico</label>
+            <input type="email" id="inputEmail" class="form-control">
+
+            <label for="password">Password</label>
+            <input type="password" id="inputPassword" class="form-control">
+
+            <label for="age">Edad</label>
+            <input type="number" name="age" id="inputAge" class="form-control">
+
+            <label for="gender">Genero</label>
+            <input type="text" name="gender" id="inputGender" class="form-control">
+
+            <label for="password">Password</label>
+            <input type="password" id="inputPassword" class="form-control">
+
+            <label for="bio">Bio</label>
+            <textarea name="bio" id="inputBio" class="form-control"></textarea>
+            <br>
+            <button class="btn btn-lg">Cancelar</button>
+            <button class="btn btn-lg btn-primary pull-right" type="submit">Registrar</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
