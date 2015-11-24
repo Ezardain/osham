9057070204afd9fa2603901fb4f9af61ef0ac017
Eduardo Zardain <ejzardainc@gmail.com>
2015-11-24 16:55:20 -0600

    Ajusta cosas del perfil

--- profile.php
+++ profile.php
@@ -16,12 +16,14 @@ $usuario = retrieveUserData("prueba@prueba.com");
 <head>
     <meta charset="UTF-8">
     <link href="../osham/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../osham/css/profile.css" rel="stylesheet">
 
     <title>Perfil</title>
 
 </head>
 <body>
-<div class="container">
+
+<!--<div class="container">
     <div class="col-md-8">
         <h1><?php echo $usuario["nombre"]?></h1>
         <h4><?php echo $usuario["edad"]?></h4>
@@ -36,6 +38,36 @@ $usuario = retrieveUserData("prueba@prueba.com");
         <div class="col-md-12">
             <button class="btn btn-lg btn-primary center-block">Editar</button>
         </div>
+    </div>
+</div>-->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="card hovercard">
+                <div class="cardheader" style="background-image: url("data:image/jpeg;base64,<?php echo base64_encode($usuario["imagenPortada"]); ?>");">
+
+                </div>
+                <div class="avatar">
+                    <img src="data:image/jpeg;base64,<?php echo base64_encode($usuario["imagenPerfil"]); ?>" />
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <?php echo $usuario["nombre"]?>
+                    </div>
+                    <div class="desc"><?php echo $usuario["edad"]?> <?php echo $usuario["genero"]?></div>
+                    <div class="desc"><?php echo $usuario["bio"]?></div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="editar_perfil.php?correo=<?php $usuario["correo"]?>">
+                        Editar
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 </div>
 </body>
